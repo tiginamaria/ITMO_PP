c9b4c7a964007c2f862d27a93056a114d042a9cb
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2021-12-20 00:20:39 +0300

    Fix task5

--- hw2/task5.cpp
+++ hw2/task5.cpp
@@ -5,15 +5,24 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	double time_start, time_finish;
-	int rank, i;
-	int len;
-	char *name = new char;
+	int len, rank, n, i, message;
+	char *name = new char();
 	MPI_Init(&argc, &argv);
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(name, &len);
 	time_start = MPI_Wtime();
-	for (i = 0; i < NTIMES; i++)
-		time_finish = MPI_Wtime();
+	for (i = 0; i < NTIMES; i++) {
+	    if (rank == 1) {
+            cout << "Hello from process " << rank << "\n";
+            for (i = 1; i < n; i++) {
+                MPI_Recv(&message, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                cout << "Hello from process " << message << endl;
+            }
+        } else MPI_Send(&rank, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+	time_finish = MPI_Wtime();
 	cout << "processor " << name << ", process " << rank << "time = " << (time_finish - time_start) / NTIMES << endl;
 	MPI_Finalize();
 }
